88df16b75e49afbda7268577dd8a125bc8d114da
Andre Hochi <andrehochiwork@gmail.com>
2021-02-13 23:05:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,39 +58,31 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-	programmer_hash = 
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      }
-    }
-  programmer_hash[:yukihiro_matsumoto] = {
-end
-	programmer_hash = 
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      }
-    }
+# add the following information to the top level of programmer_hash
+# :yukihiro_matsumoto => 
+# return the entire updated hash
 
+	programmer_hash = 
+ 		{
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
+  programmer_hash["yukihiro_matsumoto"] = {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+    
+  }
+  programmer_hash
 end
 
 def changing_alan
